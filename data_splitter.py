00e5670d0a64fc7d130f67e94d0f7a52bb7558fc
paula <paulamat1>
2025-11-06 19:54:10 +0100

    Data Splitter New - 3s each raw chunk

--- data_splitter.py
+++ data_splitter.py
@@ -4,17 +4,20 @@ import soundfile as sf
 import numpy as np
 import random as rn
 
-TOP_DB = 30           
-MIN_SEG = 1.0        
-MAX_SEG = 3.0 
+TOP_DB = 35
+INTERNAL_DB = 25
 
-SEED = 1000
+MAX_SEG = 3.0       
+FRAME_LENGTH = 2048       
+HOP_LENGTH = 256   
 
-INPUT = "class0_not_allowed"
+SEED = 200
+
+INPUT = "class1_allowed"
 RAW_OUTPUT = "raw_data"
 NORM_OUTPUT = "norm_data"
-CLASS_LABEL = "class0" 
-CLASS_DIR = "class_0"
+CLASS_LABEL = "class1" 
+CLASS_DIR = "class_1"
 
 def peak_normalize(chunk):
     peak = np.max(np.abs(chunk))
@@ -22,7 +25,7 @@ def peak_normalize(chunk):
         return chunk / peak
     else:
         return chunk
-    
+   
 def save_chunk(chunk, sample_rate, category, recording_name, count):
     raw_dir = os.path.join(RAW_OUTPUT, category, CLASS_DIR, recording_name)
     norm_dir = os.path.join(NORM_OUTPUT, category, CLASS_DIR, recording_name)
@@ -33,9 +36,10 @@ def save_chunk(chunk, sample_rate, category, recording_name, count):
     out_path_raw = os.path.join(raw_dir,base_name)
     out_path_norm = os.path.join(norm_dir, base_name)
 
+    chunk = np.asarray(chunk, dtype=np.float32)
     chunkNorm = peak_normalize(chunk)
-    sf.write(out_path_raw, chunk, sample_rate)
-    sf.write(out_path_norm, chunkNorm, sample_rate)
+    sf.write(out_path_raw, chunk, sample_rate, subtype="PCM_16")
+    sf.write(out_path_norm, chunkNorm, sample_rate, subtype="PCM_16")
 
 
 def pad_chunk(chunk, sample_rate):
@@ -46,24 +50,20 @@ def pad_chunk(chunk, sample_rate):
 
 
 def sample_len_file(file_path):
-    waveform, sample_rate = lb.load(file_path, sr=16000)
+    waveform, _ = lb.load(file_path, sr=16000, mono=True) #waveform is an numpy array of audio samples
+    waveform_trim, _ = lb.effects.trim(waveform, top_db=TOP_DB, frame_length=FRAME_LENGTH, hop_length=HOP_LENGTH)
+    
+    internal_segments = lb.effects.split(waveform_trim, top_db=INTERNAL_DB, frame_length=FRAME_LENGTH, hop_length=HOP_LENGTH)
+    
+    if len(internal_segments) == 0:
+        return len(waveform_trim)
+    else:
+        chunks = []
+        for start, end in internal_segments:
+            chunks.append(waveform_trim[start:end])
+        waveform_clean = np.concatenate(chunks)
 
-    intervals = lb.effects.split(waveform, top_db=TOP_DB)
-    max_samples = int(MAX_SEG * sample_rate)
-    min_samples = int(MIN_SEG * sample_rate)
-
-    total = 0
-    for start, end in intervals:
-        seg = waveform[start:end]
-        i = 0
-        while i < len(seg):
-            chunk = seg[i:i+max_samples]
-            if len(chunk) < min_samples:
-                break
-            total += len(chunk)            
-            i += max_samples
-    return total
-
+    return len(waveform_clean)
 
 def sample_len_speaker(speakers_path):
     speaker_len = {}
@@ -81,12 +81,10 @@ def sample_len_speaker(speakers_path):
 def sample_len_per_file(speaker_dir):
     file_len = {}
     for recording_dir in os.listdir(speaker_dir):
-        total = 0
         recording_path = os.path.join(speaker_dir, recording_dir)
         for file_name in os.listdir(recording_path):
             file_path = os.path.join(recording_path, file_name)
-            total += sample_len_file(file_path)
-        file_len[file_name] = total
+            file_len[file_name] = sample_len_file(file_path)
     return file_len
 
 def assign_files(file_len):
@@ -95,8 +93,8 @@ def assign_files(file_len):
     rn.shuffle(files)
 
     total = sum(file_len.values())
-    train_amount = 0.72 * total
-    validation_amount = 0.18 * total
+    train_amount = 0.75 * total
+    validation_amount = 0.15 * total
 
     train_sum = val_sum = 0
 
@@ -120,8 +118,8 @@ def assign_speakers(speaker_len):
     rn.shuffle(speakers)
 
     total = sum(speaker_len.values())
-    train_amount = 0.8 * total
-    validation_amount = 0.16 * total
+    train_amount = 0.7 * total
+    validation_amount = 0.2 * total
 
     train_sum = val_sum = 0
 
@@ -140,55 +138,67 @@ def assign_speakers(speaker_len):
 
 
 def segment_file(file_path, category, recording_name):
-    waveform, sample_rate = lb.load(file_path, sr=16000) #waveform is an numpy array of audio samples
+    waveform, sample_rate = lb.load(file_path, sr=16000, mono=True) #waveform is an numpy array of audio samples
 
-    intervals = lb.effects.split(waveform, top_db=TOP_DB) #each interval contains [start, end] sample positions
+    waveform_trim, _ = lb.effects.trim(waveform, top_db=TOP_DB, frame_length=FRAME_LENGTH, hop_length=HOP_LENGTH)
+    internal_segments = lb.effects.split(waveform_trim, top_db=INTERNAL_DB, frame_length=FRAME_LENGTH, hop_length=HOP_LENGTH)
+
+    if len(internal_segments) == 0:
+        waveform_clean = waveform_trim
+    else:
+        chunks = []
+        for start, end in internal_segments:
+            chunks.append(waveform_trim[start:end])
+        waveform_clean = np.concatenate(chunks)
+
     max_samples = int(MAX_SEG * sample_rate)
-    min_samples = int(MIN_SEG * sample_rate)
     count = 0
-
-    for start, end in intervals:  
-        segment = waveform[start:end]
-        i = 0
-        while i < len(segment):
-            chunk = segment[i:i+max_samples]
-            if len(chunk) < min_samples:
-                break
-            chunk = pad_chunk(chunk, sample_rate)  
-            save_chunk(chunk, sample_rate, category, recording_name, count)
-            count+=1
-            i+=max_samples
+    i = 0
+    while i < len(waveform_clean):
+        chunk = waveform_clean[i:i+max_samples]
+        if len(chunk) < max_samples:
+            chunk = pad_chunk(chunk, sample_rate)   
+        save_chunk(chunk, sample_rate, category, recording_name, count)
+        count+=1
+        i+=max_samples
 
 def test_segmentation_ratio(dir, sample_len, assign_something):
-        len = sample_len(dir)
-        cat = assign_something(len)
+        len_dir = sample_len(dir)
+        cat = assign_something(len_dir)
 
         train_time = valid_time = test_time = 0
-        for something in len:
+        for something in len_dir:
             if(cat[something] == "validation_data"):
-                valid_time += len[something]
+                valid_time += len_dir[something]
             elif(cat[something] == "test_data"):
-                test_time += len[something]
+                test_time += len_dir[something]
+            elif(cat[something] == "train_data"):
+                train_time += len_dir[something]
             else:
-                train_time += len[something]
+                raise ValueError(f"Unknown category: {len_dir[something]}")
         full_time = train_time + valid_time + test_time
         print(train_time/full_time)
         print(test_time/full_time)
         print(valid_time/full_time)
 
 def main():    
-    speaker_len = sample_len_speaker(INPUT)
-    speaker_cat = assign_files(speaker_len)
-    test_segmentation_ratio(INPUT,sample_len_speaker,assign_files)
+    """speaker_len = sample_len_speaker(INPUT)
+    speaker_cat = assign_speakers(speaker_len)
+    test_segmentation_ratio(INPUT,sample_len_speaker,assign_speakers)"""
 
     for speaker in os.listdir(INPUT):
         speaker_dir = os.path.join(INPUT,speaker)
+        
+        file_len = sample_len_per_file(speaker_dir)
+        file_cat = assign_files(file_len)
+        test_segmentation_ratio(speaker_dir, sample_len_per_file, assign_files)
+
         for recording_dir in os.listdir(speaker_dir):
             recording_name = f"{recording_dir}"
             recording_path = os.path.join(speaker_dir, recording_dir)
             for file_name in os.listdir(recording_path):
                 file_path = os.path.join(recording_path, file_name)
-                segment_file(file_path, speaker_cat[speaker], recording_name)
+                segment_file(file_path, file_cat[file_name], recording_name)
 
 if __name__ == "__main__":
     main()
