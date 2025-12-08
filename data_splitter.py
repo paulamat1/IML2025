7c4929e81110eac404befce43c786aed0b812502
paula <paulamat1>
2025-12-08 22:55:25 +0100

    updated version adjusted also to augmented files

--- data_splitter.py
+++ data_splitter.py
@@ -11,13 +11,15 @@ MAX_SEG = 3.0
 FRAME_LENGTH = 2048       
 HOP_LENGTH = 256   
 
-SEED = 200
+SEED = 300
 
-INPUT = "class0_not_allowed"
-RAW_OUTPUT = "raw_data"
-NORM_OUTPUT = "norm_data"
-CLASS_LABEL = "class0" 
-CLASS_DIR = "class_0"
+INPUT = "class1_allowed"
+RAW_OUTPUT = "raw_data_2"
+NORM_OUTPUT = "norm_data_2"
+CLASS_LABEL = "class1" 
+CLASS_DIR = "class_1"
+
+AUG_SUFFIXES = ("_noise.wav", "_chop.wav", "_fast.wav", "_slow.wav")
 
 def peak_normalize(chunk):
     peak = np.max(np.abs(chunk))
@@ -26,13 +28,13 @@ def peak_normalize(chunk):
     else:
         return chunk
    
-def save_chunk(chunk, sample_rate, category, recording_name, count):
-    raw_dir = os.path.join(RAW_OUTPUT, category, CLASS_DIR, recording_name)
-    norm_dir = os.path.join(NORM_OUTPUT, category, CLASS_DIR, recording_name)
+def save_chunk(chunk, sample_rate, category,  folder_name, file_prefix, count):
+    raw_dir = os.path.join(RAW_OUTPUT, category, CLASS_DIR, folder_name)
+    norm_dir = os.path.join(NORM_OUTPUT, category, CLASS_DIR, folder_name)
     os.makedirs(raw_dir, exist_ok=True)
     os.makedirs(norm_dir, exist_ok=True)
 
-    base_name = f"{CLASS_LABEL}_{recording_name}_{count:03d}.wav"
+    base_name = f"{CLASS_LABEL}_{file_prefix}_{count:03d}.wav"
     out_path_raw = os.path.join(raw_dir,base_name)
     out_path_norm = os.path.join(norm_dir, base_name)
 
@@ -50,6 +52,10 @@ def pad_chunk(chunk, sample_rate):
 
 
 def sample_len_file(file_path):
+    name = os.path.basename(file_path)
+    if name.endswith(AUG_SUFFIXES):
+        return 0
+    
     waveform, _ = lb.load(file_path, sr=16000, mono=True) #waveform is an numpy array of audio samples
     waveform_trim, _ = lb.effects.trim(waveform, top_db=TOP_DB, frame_length=FRAME_LENGTH, hop_length=HOP_LENGTH)
     
@@ -83,6 +89,8 @@ def sample_len_per_file(speaker_dir):
     for recording_dir in os.listdir(speaker_dir):
         recording_path = os.path.join(speaker_dir, recording_dir)
         for file_name in os.listdir(recording_path):
+            if file_name.endswith(AUG_SUFFIXES):
+                continue
             file_path = os.path.join(recording_path, file_name)
             file_len[file_name] = sample_len_file(file_path)
     return file_len
@@ -118,8 +126,8 @@ def assign_speakers(speaker_len):
     rn.shuffle(speakers)
 
     total = sum(speaker_len.values())
-    train_amount = 0.8 * total
-    validation_amount = 0.14 * total
+    train_amount = 0.7 * total
+    validation_amount = 0.2 * total
 
     train_sum = val_sum = 0
 
@@ -137,7 +145,7 @@ def assign_speakers(speaker_len):
     return speaker_cat
 
 
-def segment_file(file_path, category, recording_name):
+def segment_file(file_path, category, folder_name, file_prefix):
     waveform, sample_rate = lb.load(file_path, sr=16000, mono=True) #waveform is an numpy array of audio samples
 
     waveform_trim, _ = lb.effects.trim(waveform, top_db=TOP_DB, frame_length=FRAME_LENGTH, hop_length=HOP_LENGTH)
@@ -158,7 +166,7 @@ def segment_file(file_path, category, recording_name):
         chunk = waveform_clean[i:i+max_samples]
         if len(chunk) < max_samples:
             return   
-        save_chunk(chunk, sample_rate, category, recording_name, count)
+        save_chunk(chunk, sample_rate, category, folder_name, file_prefix, count)
         count+=1
         i+=max_samples
 
@@ -189,16 +197,47 @@ def main():
     for speaker in os.listdir(INPUT):
         speaker_dir = os.path.join(INPUT,speaker)
         
-        """file_len = sample_len_per_file(speaker_dir)
+        file_len = sample_len_per_file(speaker_dir)
         file_cat = assign_files(file_len)
-        test_segmentation_ratio(speaker_dir, sample_len_per_file, assign_files)"""
+        test_segmentation_ratio(speaker_dir, sample_len_per_file, assign_files)
 
         for recording_dir in os.listdir(speaker_dir):
-            recording_name = f"{recording_dir}"
             recording_path = os.path.join(speaker_dir, recording_dir)
+
             for file_name in os.listdir(recording_path):
                 file_path = os.path.join(recording_path, file_name)
-                segment_file(file_path, speaker_cat[speaker], recording_name)
+                folder_name = recording_dir  
+                file_prefix = recording_dir  
+
+                if not file_name.endswith(AUG_SUFFIXES):
+                    category = file_cat[file_name]
+                    file_prefix = recording_dir
+                    segment_file(file_path, category, folder_name, file_prefix)
+                    continue
+
+                base_name = file_name
+                for suf in AUG_SUFFIXES:
+                    if base_name.endswith(suf):
+                        base_name = base_name.replace(suf, ".wav")
+                        break
+
+                base_cat = file_cat[base_name]
+                if base_cat != "train_data":
+                    continue
+
+                category = "train_data"
+                if file_name.endswith("_chop.wav"):
+                    variant_suffix = "_chop"
+                elif file_name.endswith("_noise.wav"):
+                    variant_suffix = "_noise"
+                elif file_name.endswith("_fast.wav"):
+                    variant_suffix = "_fast"
+                elif file_name.endswith("_slow.wav"):
+                    variant_suffix = "_slow"
+                else:
+                    variant_suffix = "_aug"
+                file_prefix = recording_dir + variant_suffix
+                segment_file(file_path, category, folder_name, file_prefix)
 
 if __name__ == "__main__":
     main()
