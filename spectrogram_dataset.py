26fb3ca994bd3c96c841dd733482c862c3f7e675
paula <paulamat1>
2025-12-09 17:33:30 +0100

    adjusted to handle augmented data

--- spectrogram_dataset.py
+++ spectrogram_dataset.py
@@ -2,9 +2,14 @@ import os
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+import random
+
 
 class NpySpectrogramDataset(Dataset):
-    def __init__(self, root, mel_bins=80, expected_width=None, class_to_label=None):
+    def __init__(self, root, mel_bins=80, expected_width=None, class_to_label=None,
+        aug_ratio=1.0, use_noise=True, use_chop=True, use_fast=True, use_slow=True):
+        random.seed(300)
+
         self.root = root
         self.mel_bins = mel_bins
         self.expected_width = expected_width
@@ -14,7 +19,9 @@ class NpySpectrogramDataset(Dataset):
         else:
             self.class_to_label = class_to_label
 
-        self.samples = []
+        original_samples = []
+        aug_samples = []
+
         for class_name, label in self.class_to_label.items():
             class_dir = os.path.join(root, class_name)
             for speaker in os.listdir(class_dir):
@@ -22,10 +29,28 @@ class NpySpectrogramDataset(Dataset):
                 for filename in os.listdir(speaker_dir):
                     if filename.endswith(".npy"):
                         full_path = os.path.join(speaker_dir, filename)
-                        self.samples.append((full_path, label))
-        
+                        if filename.endswith("_noise.npy"):
+                            if use_noise:
+                                aug_samples.append((full_path, label))
+                        elif filename.endswith("_chop.npy"):
+                            if use_chop:
+                                aug_samples.append((full_path, label))
+                        elif filename.endswith("_fast.npy"):
+                            if use_fast:
+                                aug_samples.append((full_path, label))
+                        elif filename.endswith("_slow.npy"):
+                            if use_slow:
+                                aug_samples.append((full_path, label))
+                        else:
+                            original_samples.append((full_path, label))
+
+        if 0.0 <= aug_ratio < 1.0 and len(aug_samples) > 0:
+            k = int(round(len(aug_samples) * aug_ratio))
+            aug_samples = random.sample(aug_samples, k)
+
+        self.samples = original_samples + aug_samples
         self.samples.sort()
-    
+
     def __len__(self):
         return len(self.samples)
 
@@ -33,7 +58,7 @@ class NpySpectrogramDataset(Dataset):
         path, label = self.samples[index]
         arr = np.load(path)
 
-        if(arr.ndim !=2):
+        if arr.ndim != 2:
             arr = np.squeeze(arr)
 
         H, W = arr.shape
@@ -42,7 +67,6 @@ class NpySpectrogramDataset(Dataset):
         if W != self.expected_width:
             raise ValueError(f"{path} width {W}, expected {self.expected_width}")
         
-        input = torch.from_numpy(arr.astype(np.float32)).unsqueeze(0)  # [1, 80, W]
-        label = torch.tensor(label, dtype=torch.long)
-        return input, label
-            
+        x = torch.from_numpy(arr.astype(np.float32)).unsqueeze(0)  # [1, 80, W]
+        y = torch.tensor(label, dtype=torch.long)
+        return x, y
